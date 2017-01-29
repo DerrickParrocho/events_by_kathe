97121d99f08d1e7adf1f7b7eabf5d9e5550cb134
derrick <derrickparrocho17@gmail.com>
2017-01-29 04:40:04 +0000

    added services and contact us form

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   
 
   root 'pages#home'
-    get '/gallery' => 'pages#gallery'
+  get '/gallery' => 'pages#gallery'
+  get '/services' => 'pages#services'
+  get '/contact' => 'pages#contact'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
