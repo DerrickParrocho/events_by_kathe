33a3798913305198cdb2b3edb564532aec529009
derrick <derrickparrocho17@gmail.com>
2017-01-31 03:12:35 +0000

    improved contacts form

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   root 'pages#home'
   get '/gallery' => 'pages#gallery'
   get '/services' => 'pages#services'
-  #get '/contact' => 'pages#contact'
   resources :contacts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
